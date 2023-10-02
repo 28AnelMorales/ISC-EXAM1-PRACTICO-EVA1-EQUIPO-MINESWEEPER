97093317b566acaf7a5a535ffadd208eaa4f7b6f
28AnelMorales <143563398+28AnelMorales@users.noreply.github.com>
2023-10-01 21:49:56 -0700

    Add files via upload

---
+++ PROBLEMA1/src/problema1/EXA-PROBLEMA1.java
@@ -0,0 +1,31 @@
+
+package problema1;
+
+
+
+public class PROBLEMA1 {
+
+   
+    public static void main(String[] args) {
+    
+      //nombre del equipo
+      String a ="MINESWEEPER";
+      //nombre proyecto final
+      String b ="MINESWEEPER";
+      //integrantes
+      String c ="María Anel Morales Vidaca 2355390";
+      String d ="Angela Jaqueline Duarte Mercado 23550370";
+      String e ="Dulce Naomi Perez Franco 23550339";
+      String f ="Emily cedillo Sánchez 23550413";
+      
+      //impresion de variable
+        System.out.println("El equipo es: "+a);
+        
+        System.out.println("El nombre del proyecto final es: "+b);
+        
+        System.out.println("Los integrantes son: \n"+c+"\n"+d+"\n "+e+"\n "+f);
+      
+        
+    }
+    
+}
